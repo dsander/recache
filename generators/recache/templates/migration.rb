8493964a1de50aa1e48717058226c8fbd71898e6
U-GOTIMBA\DePairet <DePairet@gotimba.(none)>
2008-08-17 22:09:38 +0200

    changed migration to support large cache data (probably only working for mysql)

--- generators/recache/templates/migration.rb
+++ generators/recache/templates/migration.rb
@@ -2,9 +2,9 @@ class CreateRecacheStores < ActiveRecord::Migration
   def self.up
     create_table :recache_stores do |t|
       t.string :request_hash
-      t.text :data
       t.datetime :cached_until
     end
+	t.columns << 'data longtext'
     add_index :recache_stores, :request_hash
   end
 
